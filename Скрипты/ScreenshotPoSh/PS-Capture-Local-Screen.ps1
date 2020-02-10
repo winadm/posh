f3d1cbcb54aec7461da83554c8bc032a9dca3bd0
winadm <kbuldogov@gmail.com>
2020-02-10 15:35:23 +0500

    PowerShell ScreenShot

---
+++ Скрипты/ScreenshotPoSh/PS-Capture-Local-Screen.ps1
@@ -0,0 +1,21 @@
+$Path = "C:\ps\screenshots"
+# Проверяем, что каталог для хранения скриншотов создан, если нет - создаем его
+If (!(test-path $path)) {
+    New-Item -ItemType Directory -Force -Path $path
+}
+Add-Type -AssemblyName System.Windows.Forms
+$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+# Получаем разрешение экрана
+$image = New-Object System.Drawing.Bitmap($screen.Width, $screen.Height)
+# Создаем графический объект
+$graphic = [System.Drawing.Graphics]::FromImage($image)
+$point = New-Object System.Drawing.Point(0, 0)
+$graphic.CopyFromScreen($point, $point, $image.Size);
+$cursorBounds = New-Object System.Drawing.Rectangle([System.Windows.Forms.Cursor]::Position, [System.Windows.Forms.Cursor]::Current.Size)
+# Получаем скриншот экрана 
+[System.Windows.Forms.Cursors]::Default.Draw($graphic, $cursorBounds)
+$screen_file = "$Path\" + $env:computername + "_" + $env:username + "_" + "$((get-date).tostring('yyyy.MM.dd-HH.mm.ss')).png"
+# Сохранить скриншот в файл
+$image.Save($screen_file, [System.Drawing.Imaging.ImageFormat]::Png)
+
+
